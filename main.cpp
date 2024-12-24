f8ec0d7528d0005e685b72e91f8b6d852d6a06a5
Irene Mizus <irene.mizus@gmail.com>
2024-12-24 17:34:21 +0200

    Renamed k(K) <-> n(N)

--- main.cpp
+++ main.cpp
@@ -6,14 +6,14 @@
 
 void multiply_v0(const float* __restrict__ a, const float* __restrict__ bT, float* __restrict__ c, int M, int K, int N) {
     for (int m = 0; m < M; m++) {
-        for (int k = 0; k < K; k++) {
-            c[m * K + k] = 0.0;
-            for (int n = 0; n < N; n++) {
-                // c[m, k] += a[m, n] * b[n, k]
-                // c[m, k] = c[m * K + k]
-                // a[m, n] = a[m * N + n]
-                // b[n, k] = b[n * K + k]
-                c[m * K + k] += a[m * N + n] * bT[k * N + n];
+        for (int n = 0; n < N; n++) {
+            c[m * N + n] = 0.0;
+            for (int k = 0; k < K; k++) {
+                // c[m, n] += a[m, k] * bT[n, k]
+                // c[m, n] = c[m * N + n]
+                // a[m, k] = a[m * K + k]
+                // bT[n, k] = bT[n * K + k]
+                c[m * N + n] += a[m * K + k] * bT[n * K + k];
             }
         }
     };
@@ -21,18 +21,13 @@ void multiply_v0(const float* __restrict__ a, const float* __restrict__ bT, floa
 
 void multiply_v1(const float* __restrict__ a, const float* __restrict__ bT, float* __restrict__ c, int M, int K, int N) {
     for (int m = 0; m < M; m++) {
-        for (int k = 0; k < K; k++) {
-            c[m * K + k] = 0.0;
-            for (int n1 = 0; n1 < N; n1 += 16) {
-                // c[m, k] += a[m, n] * b[n, k]
-                // c[m, k] = c[m * K + k]
-                // a[m, n] = a[m * N + n]
-                // b[n, k] = b[n * K + k]
-
-                const float* pa = &a[m * N + n1];
-                const float* pb = &bT[k * N + n1];
-                for (int n2 = 0; n2 < 16; n2++) {
-                    c[m * K + k] += pa[n2] * pb[n2];
+        for (int n = 0; n < N; n++) {
+            c[m * N + n] = 0.0;
+            for (int k1 = 0; k1 < K; k1 += 16) {
+                const float* pa = &a[m * K + k1];
+                const float* pb = &bT[n * K + k1];
+                for (int k2 = 0; k2 < 16; k2++) {
+                    c[m * N + n] += pa[k2] * pb[k2];
                 }
             }
         }
@@ -41,15 +36,11 @@ void multiply_v1(const float* __restrict__ a, const float* __restrict__ bT, floa
 
 void multiply_v11(const float* __restrict__ a, const float* __restrict__ bT, float* __restrict__ c, int M, int K, int N) {
     for (int m = 0; m < M; m++) {
-        for (int k = 0; k < K; k++) {
-            c[m * K + k] = 0.0;
-            for (int n1 = 0; n1 < N; n1 += 16) {
-                // c[m, k] += a[m, n] * b[n, k]
-                // c[m, k] = c[m * K + k]
-                // a[m, n] = a[m * N + n]
-                // b[n, k] = b[n * K + k]
-                for (int n2 = 0; n2 < 16; n2++) {
-                    c[m * K + k] += a[m * N + n1 + n2] * bT[k * N + n1 + n2];
+        for (int n = 0; n < N; n++) {
+            c[m * N + n] = 0.0;
+            for (int k1 = 0; k1 < K; k1 += 16) {
+                for (int k2 = 0; k2 < 16; k2++) {
+                    c[m * N + n] += a[m * K + k1 + k2] * bT[n * K + k1 + k2];
                 }
             }
         }
@@ -60,24 +51,24 @@ bool epsilon_equal(float a, float b) {
     return std::abs(a - b) < 1e-4;
 }
 
-void print_mat(const float* c, int M, int K) {
+void print_mat(const float* c, int M, int N) {
     for (int i = 0; i < M; i++) {
-        for (int j = 0; j < K; j++) {
-            std::cout << c[i * K + j] << " ";
+        for (int j = 0; j < N; j++) {
+            std::cout << c[i * N + j] << " ";
         }
         std::cout << std::endl;
     }
 }
 
 void tiny_test() {
-    int M = 3, N = 2, K = 4;
-    std::vector<float> va = { 1, 2, 3, 4, 5, 6 };           // M * N
-    std::vector<float> vbT = { 6, 5, 4, 3, 2, 1, 1, 2 };    // K * N
-    std::vector<float> vc(M * K);
+    int M = 3, K = 2, N = 4;
+    std::vector<float> va = { 1, 2, 3, 4, 5, 6 };           // M * K
+    std::vector<float> vbT = { 6, 5, 4, 3, 2, 1, 1, 2 };    // N * K
+    std::vector<float> vc(M * N);
 
     multiply_v0(va.data(), vbT.data(), vc.data(), M, K, N);
 
-    print_mat(vc.data(), M, K);
+    print_mat(vc.data(), M, N);
 }
 
 int main() {
@@ -95,49 +86,49 @@ int main() {
     int N = 128;
 
     float *a;
-    std::vector<float> va(M * N);
+    std::vector<float> va(M * K);
     for (float& f : va) {
         f = dist(e2);
     }
     a = va.data();
 
     float *bT;
-    std::vector<float> vbT(K * N);
+    std::vector<float> vbT(N * K);
     for (float& f : vbT) {
         f = dist(e2);
     }
     bT = vbT.data();
 
     float *c;
-    std::vector<float> vc(M * K);
+    std::vector<float> vc(M * N);
     c = vc.data();
 
 //    Trying to do alignment...
 //
-//    float* a = (float*)aligned_alloc(64, M * N * sizeof(float));
-//    for (int i = 0; i < M * N; i++) {
+//    float* a = (float*)aligned_alloc(64, M * K * sizeof(float));
+//    for (int i = 0; i < M * K; i++) {
 //        a[i] = dist(e2);
 //    }
-//    float* bT = (float*)aligned_alloc(64, K * N * sizeof(float));
-//    for (int i = 0; i < K * N; i++) {
+//    float* bT = (float*)aligned_alloc(64, N * K * sizeof(float));
+//    for (int i = 0; i < N * K; i++) {
 //        bT[i] = dist(e2);
 //    }
-//    float* c = (float*)aligned_alloc(64, M * K * sizeof(float));
-//    float* c1 = (float*)aligned_alloc(64, M * K * sizeof(float));
+//    float* c = (float*)aligned_alloc(64, M * N * sizeof(float));
+//    float* c1 = (float*)aligned_alloc(64, M * N * sizeof(float));
 
 
     std::chrono::time_point time_1 = std::chrono::system_clock::now();
-    multiply_v0(a, bT, c, M, K ,N);
+    multiply_v0(a, bT, c, M, K, N);
     std::chrono::time_point time_2 = std::chrono::system_clock::now();
 
     auto nanosec = std::chrono::duration_cast<std::chrono::nanoseconds>(time_2 - time_1).count();
 
     //std::cout << "Matrix multiplication result: " << std::endl;
-    //print_mat(c, M, K);
+    //print_mat(c, M, N);
     std::cout << "Matrix multiplication naive: " << nanosec * 1e-6 << " ms" << std::endl;
 
     float *c1;
-    std::vector<float> vc1(M * K);
+    std::vector<float> vc1(M * N);
     c1 = vc1.data();
 
     std::chrono::time_point time_3 = std::chrono::system_clock::now();
